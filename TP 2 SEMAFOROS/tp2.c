4a7c9640c0f599d2fc8d8edc03885f58747bd347
Franco <frankomendoza78@gmail.com>
2024-09-08 23:02:44 -0300

    tp problemas con mozos servircomida

--- TP 2 SEMAFOROS/tp2.c
+++ TP 2 SEMAFOROS/tp2.c
@@ -14,8 +14,9 @@ sem_t sem_platoTerminado;
 sem_t sem_preguntaLanzada;
 sem_t sem_respuestaLanzada;
 sem_t sem_seLevantoManucho;
+sem_t sem_platosDisponibles;
+int cantSentados = 0;
 
-// Funciones modificadas con sleep() y mensajes
 void HacerAsado() {
     printf("Asador está preparando el asado...\n");
 //    sleep(5);  // Simula el tiempo para hacer el asado
@@ -69,6 +70,10 @@ void* manucho(void* arg) {
 	int N = *((int*) arg);
     sem_wait(&sem_invitadosSentados); // Espera a que todos los invitados estén sentados
     Sentarse();  // Manucho se sienta
+	for (int i = 0; i < N; i++) {
+        sem_post(&sem_platosDisponibles); // Señala que Manucho se sentó
+    }
+	printf("se sento manucho... \n");
 	
 	for (int i = 0; i < N; i++) {
         sem_post(&sem_manuchoSentado); // Señala que Manucho se sentó
@@ -76,7 +81,8 @@ void* manucho(void* arg) {
 	
     sem_wait(&sem_comidaServida); // Espera a que le sirvan la comida
     Comer();  // Manucho come
-    sem_post(&sem_manuchoTerminaDeComer); // Señala que terminó de comer
+	printf("manucho termino de comer \n");
+//    sem_post(&sem_manuchoTerminaDeComer); // Señala que terminó de comer
 
     Lanzar_pregunta_mundialista();  // Lanza la pregunta sobre el Mundial
     sem_post(&sem_preguntaLanzada); // Señala que la pregunta fue lanzada
@@ -90,11 +96,16 @@ void* manucho(void* arg) {
 
 // Función de los Invitados
 void* invitado(void* arg) {
+	int N = *((int*) arg);
     Sentarse();  // El invitado se sienta
-    sem_post(&sem_invitadosSentados); // Señala que un invitado se ha sentado
-
+    cantSentados++;
+	printf("%d",cantSentados);
+	if (cantSentados==N){
+		sem_post(&sem_invitadosSentados); // Señala que todos sentados
+	}	
     sem_wait(&sem_comidaServida);  // Espera a que le sirvan la comida
     Comer();  // Come la comida
+	printf("invitado termina de comer... \n");
     sem_post(&sem_platoTerminado); // Señala que terminó de comer
 
     sem_wait(&sem_preguntaLanzada);  // Espera a que Manucho haga la pregunta
@@ -109,10 +120,13 @@ void* invitado(void* arg) {
 // Función de los Mozos
 void* mozo(void* arg) {
     sem_wait(&sem_asadoListo); // Espera a que el asado esté listo
-    sem_wait(&sem_manuchoSentado); // Espera a que Manucho esté sentado
-
+//    sem_wait(&sem_manuchoSentado); // Espera a que Manucho esté sentado
+	sem_wait(&sem_platosDisponibles);
+	
     ServirComida();  // Sirve la comida
+	
     sem_post(&sem_comidaServida);  // Señala que se ha servido comida
+	printf("sirvio comida \n");
     pthread_exit(NULL);
 }
 
@@ -128,15 +142,16 @@ int main() {
     M = N - 1;
 
     // Inicialización de semáforos
-    sem_init(&sem_asadoListo, 0, 0);
-    sem_init(&sem_invitadosSentados, 0, 0);
-    sem_init(&sem_manuchoSentado, 0, 0);
-    sem_init(&sem_comidaServida, 0, 0);
-    sem_init(&sem_manuchoTerminaDeComer, 0, 0);
-    sem_init(&sem_platoTerminado, 0, 0);
-    sem_init(&sem_preguntaLanzada, 0, 0);
-    sem_init(&sem_respuestaLanzada, 0, 0);
-    sem_init(&sem_seLevantoManucho, 0, 0);
+    sem_init(&sem_asadoListo, 1, 0);
+    sem_init(&sem_invitadosSentados, 1, 0);
+    sem_init(&sem_manuchoSentado, 1, 0);
+    sem_init(&sem_comidaServida, 1, 0);
+    sem_init(&sem_manuchoTerminaDeComer, 1, 0);
+    sem_init(&sem_platoTerminado, 1, 0);
+    sem_init(&sem_preguntaLanzada, 1, 0);
+    sem_init(&sem_respuestaLanzada, 1, 0);
+    sem_init(&sem_seLevantoManucho, 1, 0);
+	sem_init(&sem_platosDisponibles, 1, 0);
 
     pthread_t thread_asador, thread_manucho;
     pthread_t thread_invitados[N]; // N es el número de invitados
@@ -147,8 +162,10 @@ int main() {
     pthread_create(&thread_manucho, NULL, manucho, (void *) &N);
 
     for (int i = 0; i < N; i++) {
-        pthread_create(&thread_invitados[i], NULL, invitado, NULL);
+        pthread_create(&thread_invitados[i], NULL, invitado, (void *) &N);
     }
+	
+
     for (int i = 0; i < M; i++) {
         pthread_create(&thread_mozos[i], NULL, mozo, NULL);
     }
@@ -174,6 +191,7 @@ int main() {
     sem_destroy(&sem_preguntaLanzada);
     sem_destroy(&sem_respuestaLanzada);
     sem_destroy(&sem_seLevantoManucho);
+	sem_destroy(&sem_platosDisponibles);
 
     return 0;
 }
